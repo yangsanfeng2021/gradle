40e76048cf22dc8351928e64cf579a9b5e33c9f0
Jendrik Johannes <jendrik@gradle.com>
2020-12-09 19:16:32 +0100

    Ignored and slow projects are identified by name only

--- .teamcity/Gradle_Check/model/GradleSubprojectProvider.kt
+++ .teamcity/Gradle_Check/model/GradleSubprojectProvider.kt
@@ -23,9 +23,9 @@ import model.TestCoverage
 import java.io.File
 
 val ignoredSubprojects = listOf(
-    "subprojects:soak", // soak test
-    "subprojects:distributions-integ-tests", // build distribution testing
-    "subprojects:architecture-test" // sanity check
+    "soak", // soak test
+    "distributions-integ-tests", // build distribution testing
+    "architecture-test" // sanity check
 )
 
 interface GradleSubprojectProvider {
